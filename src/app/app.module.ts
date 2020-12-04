f73c05f03218ad20cd3974d005d0cdb2937f5296
Zevi Liao <jointjm@gmail.com>
2020-12-04 11:11:00 +0800

    add ngrx counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Zdemo1Module } from './zdemo1/zdemo1.module';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from '../store/counter';
+import { Comp2Component } from './comp2/comp2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    Zdemo1Module
+    Zdemo1Module,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
